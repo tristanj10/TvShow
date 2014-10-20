6baf5c32919d3495ddd317f2f3f312020f4b1489
Tristan <tristan.jougier@gmail.com>
2014-10-20 17:10:14 +0200

    initial commit

---
+++ showtrackr/server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+// view engine setup
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.set('port', process.env.PORT || 3000);
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//app.use('/', routes);
+//app.use('/users', users);
+
+app.listen(app.get('port'), function() {
+	console.log('Express server listening on port ' + app.get('port'));
+}):;
