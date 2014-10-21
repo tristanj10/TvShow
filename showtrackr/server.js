554beff7a76aaa39c24c2f3420ca7a50b6f59443
Tristan <tristan.jougier@gmail.com>
2014-10-21 16:20:22 +0200

    Commit 5

--- showtrackr/server.js
+++ showtrackr/server.js
@@ -24,4 +24,4 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(app.get('port'), function() {
 	console.log('Express server listening on port ' + app.get('port'));
-}):;
+});
